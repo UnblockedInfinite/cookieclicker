2e397587a1ac7453fd6ca2b2e3892889c6d50da7
UnblockedInfinite <115199306+UnblockedInfinite@users.noreply.github.com>
2022-10-08 11:49:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15893,7 +15893,7 @@ window.onload=function()
 				LoadLang('loc/'+lang+'.js?v='+Game.version,function(){
 					var launch=function(){
 						Game.Launch();
-						if (top!=self) print("IFramed");
+						if (top!=self) Game.Launch();
 						else
 						{
 							console.log('[=== '+choose([
